ecd493a263c8be37316c25cebfbb9b6bf61e82f6
Edwin Amaranto Villegas <63481204+EdwinCoder85@users.noreply.github.com>
2023-12-18 01:43:29 -0500

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+import changeLanguage from "./helpers/changeLanguage.js";
+import switchTheme from "./helpers/switchTheme.js";
+import showMenu from "./helpers/showMenu.js";
+
+/* UI Element */
+
+/* Change Language */
+changeLanguage()
+
+/* Show Menu */
+showMenu()
+
+/* Switch Theme */
+switchTheme()
+
+/* End UI Element */
